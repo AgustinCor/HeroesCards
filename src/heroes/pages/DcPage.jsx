cd71e4aeddfcda72dff5b135939d6e4d31e89d0b
AgustinCor <agustin.cordero@gmail.com>
2023-04-25 23:20:20 -0300

    first commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,14 @@
+import HeroList from "../components/HeroList";
+
+const DcPage = () => {
+    return (
+        <>
+          <h1>Dc Page</h1>
+        <hr />
+
+        <HeroList publisher="DC Comics"/>
+        </>
+    );
+};
+
+export default DcPage;
---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,13 @@
+import HeroList from "../components/HeroList";
+
+
+const MarvelPage = () => {
+    return (
+        <>
+        <h1>MarvelPage</h1>
+        <HeroList publisher="Marvel Comics"/>
+        </>
+    );
+};
+
+export default MarvelPage;
